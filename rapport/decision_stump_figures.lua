e8d8b16b4306d70aa54a6d1fc9f0ad5d3f7b0881
Julien Dethurens <julien.dethurens.1@ulaval.ca>
2018-12-02 22:22:46 -0500

    Use commas in the figure with random forest scores

--- rapport/decision_stump_figures.lua
+++ rapport/decision_stump_figures.lua
@@ -1,11 +1,10 @@
-for i = 1, 3 do
-	local n_estimators = i * 16
-	tex.print(string.format([[
+for n_estimators = 16, 50, 16 do
+	tex.print(([[
 		\begin{figure}
 			\centering
 			\input{figures/3a/decision_stumps/decision_regions_%d.pgf}
 			\caption{Régions de décision générées par AdaBoost avec des souches de décision et \numprint{%d} classifieurs}
 			\label{fig:adaboost_decision_regions_with_decision_stumps_%d}
 		\end{figure}
-	]], n_estimators, n_estimators, n_estimators))
+	]]):format(n_estimators, n_estimators, n_estimators))
 end
